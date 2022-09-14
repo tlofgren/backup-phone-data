53d798e5a50069cf17f0867459f0339279371e10
Tyler Lofgren <tlofgren@users.noreply.github.com>
2022-09-14 02:20:08 -0500

    Don't backup Keepass

--- backup_galaxy.sh
+++ backup_galaxy.sh
@@ -226,7 +226,7 @@ fi
 
 #####
 # Folders on Main Storage to keep after backup
-MOTO_TOP_LEVEL_DIRS=( "AMdroid" "beam" "Documents" "Download" "games" "Keepass" "Playlists" "SmsContactsBackup" "Snapchat" "Voicemails" "WhatsApp" )
+MOTO_TOP_LEVEL_DIRS=( "AMdroid" "beam" "Documents" "Download" "games" "Playlists" "SmsContactsBackup" "Snapchat" "Voicemails" "WhatsApp" )
 for dir in "${MOTO_TOP_LEVEL_DIRS[@]}"; do
     # TODO: pull each folder in list
     TOP_LEVEL_PATH="${MOTO_MAIN_STORAGE_DIR}/${dir}"
